2e8e0a43080acd21b8e3ba20abc8bd17e058c41c
Pukarst <bade5pukar@gmail.com>
2026-04-12 23:12:41 +0545

    Add introductory content for Python programming

---
+++ i.Python_basics/1_python_baiscs/1.0_python_introduction.py
@@ -0,0 +1,20 @@
+'''
+======= Introduction to Python Programming =======
+
+-created by Guido Van Rossum and released in 1991 A.D
+-named after the British comedy group Monty Python
+-choose this name because of short,unique and memorable
+-one of the most popular programming language with wide range of application and systems
+-powerful and flexible language 
+-easy to learn and understand 
+-high level,interpreted programming language for it's simplicity and readability
+-provide big library support
+-works on different platform like windows,mac,linux,etc
+-open source programming language
+-simple syntax similar to the English language
+-can handle big data and perfom the complex mathematics
+-runs on an interpreter system (code will execute as it is written)
+-can be treated in procedural,object-oriented or functional ways
+-possible to integrate with other programming language
+
+'''
