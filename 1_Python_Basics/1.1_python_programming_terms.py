d8c6737270e7b311c8b8937a69b10e7ddd242d58
Pukarst <bade5pukar@gmail.com>
2026-04-14 23:56:40 +0545

    Add Python programming terms explanation

---
+++ 1_Python_Basics/1.1_python_programming_terms.py
@@ -0,0 +1,36 @@
+'''
+=======>> Python Programming Terms <<=======
+
+-keywords: They are the reserved words which have the special meanings and are used to perform 
+            specific tasks.We can't use them as the variables,functions or identifiers.
+
+-variables: Is the name used to store the data or values.It acts like the container for the 
+            information.Variable name should start with letter or underscore, not number.
+            They are case sensitive.
+            There are different techniques of naming the variables:
+                a.Camel case: Each word,except first, start with the capital letter.
+                i.e: myVariableName='Python'
+
+                b.Pascal case: Each word starts with the capital letter.
+                i.e: MyVariableName='Python'
+
+                c.Snake case: Each word is separated by underscore.
+                i.e:my_variable_name='Python'
+                    OR
+                    MY_VARIABLE_NAME='Python'
+
+-Functions: It is the block of code that perform the specific tasks and helps to reuse the code.
+            It makes the code organized and easy to read.
+
+-Classes: Class is the blueprint for creating objects.It groups the data and functions
+          together.
+
+-Objects: It is an instance of class which represent real world entity.Each object has different valuses.
+
+-Modules:They are files that contain Python code such as functions,variables,classes allowing code to be
+        organized and reused.
+
+-Packages: They are the collections of related modules grouped together in directories, helping manage
+            large and complex projects.
+
+'''
